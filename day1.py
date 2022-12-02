a382837cbd6bbb2293400058fb6568b51999ab4f
ricano <depoisdizcomofoi@gmail.com>
2022-12-02 09:51:33 +0000

    day1

---
+++ day1.py
@@ -0,0 +1,42 @@
+from inputs import d1_list
+
+
+def resolve_day1():
+    number_of_elves = 0
+    total_calories = 0
+    calories_per_elf = [[]]
+
+    for item in d1_list:
+        if item != "":
+            calories_per_elf[number_of_elves].append(item)
+            total_calories += int(item)
+        else:
+            number_of_elves += 1
+            calories_per_elf.append([])
+
+    total_calories_per_elf = []
+
+    for item in calories_per_elf:
+        sum = 0
+        for value in item:
+            sum += int(value)
+        if sum == 71780:
+            print("index:", calories_per_elf.index(item))
+        total_calories_per_elf.append(sum)
+
+    max = 0
+    for x in total_calories_per_elf:
+        if int(x) > max:
+            max = int(x)
+
+    total_calories_per_elf.sort(reverse=True)
+    top3 = total_calories_per_elf[0]+total_calories_per_elf[1]+total_calories_per_elf[2]
+
+    print("number_of_elves: ", number_of_elves + 1)
+    print("total_calories:", total_calories)
+    print("calories_per_elf_list:", calories_per_elf)
+    print("len_calories_per_elf_list:", len(calories_per_elf))
+    print("total_calories_per_elf:", total_calories_per_elf)
+    print("len_total_calories_per_elf:", len(total_calories_per_elf))
+    print("MAX TOTAL CALORIES CARRIED BY ONE ELF: ", max)
+    print("TOTAL CALORIES CARRIED BY TOP 3 ELVES: ", top3)
---
+++ main.py
@@ -0,0 +1,12 @@
+from day1 import resolve_day1
+
+DAY = 1
+
+
+def resolve(day):
+    if day == 1:
+        resolve_day1()
+
+
+if __name__ == '__main__':
+    resolve(DAY)
