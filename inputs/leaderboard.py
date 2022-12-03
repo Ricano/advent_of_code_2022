42bb40e34eaf213ac9ace496a49761551bb19b53
ricano <depoisdizcomofoi@gmail.com>
2022-12-03 13:38:20 +0000

    add leaderboard

---
+++ inputs/leaderboard.py
@@ -0,0 +1,239 @@
+leaderboard = {
+    "owner_id": 745454,
+    "event": "2022",
+    "members": {
+        "745454": {
+            "stars": 2,
+            "completion_day_level": {
+                "1": {
+                    "1": {
+                        "star_index": 171063,
+                        "get_star_ts": 1669945170
+                    },
+                    "2": {
+                        "star_index": 290863,
+                        "get_star_ts": 1669974661
+                    }
+                }
+            },
+            "global_score": 0,
+            "name": "Rui Rei",
+            "local_score": 9,
+            "id": 745454,
+            "last_star_ts": 1669974661
+        },
+        "806047": {
+            "completion_day_level": {
+
+            },
+            "global_score": 0,
+            "name": "Gustavo Lima",
+            "stars": 0,
+            "last_star_ts": 0,
+            "id": 806047,
+            "local_score": 0
+        },
+        "1719951": {
+            "name": "Diogo Bastos",
+            "completion_day_level": {
+                "1": {
+                    "1": {
+                        "star_index": 282853,
+                        "get_star_ts": 1669973259
+                    },
+                    "2": {
+                        "get_star_ts": 1669973512,
+                        "star_index": 284386
+                    }
+                },
+                "2": {
+                    "1": {
+                        "get_star_ts": 1669975312,
+                        "star_index": 294419
+                    },
+                    "2": {
+                        "get_star_ts": 1669975550,
+                        "star_index": 295676
+                    }
+                }
+            },
+            "global_score": 0,
+            "stars": 4,
+            "local_score": 23,
+            "id": 1719951,
+            "last_star_ts": 1669975550
+        },
+        "1832445": {
+            "completion_day_level": {
+                "1": {
+                    "1": {
+                        "star_index": 0,
+                        "get_star_ts": 1669889769
+                    },
+                    "2": {
+                        "get_star_ts": 1669889951,
+                        "star_index": 682
+                    }
+                },
+                "2": {
+                    "1": {
+                        "star_index": 275966,
+                        "get_star_ts": 1669972080
+                    },
+                    "2": {
+                        "star_index": 282815,
+                        "get_star_ts": 1669973253
+                    }
+                },
+                "3": {
+                    "1": {
+                        "star_index": 559768,
+                        "get_star_ts": 1670057249
+                    },
+                    "2": {
+                        "get_star_ts": 1670057987,
+                        "star_index": 562643
+                    }
+                }
+            },
+            "name": "Diogo Santos",
+            "global_score": 0,
+            "stars": 6,
+            "last_star_ts": 1670057987,
+            "local_score": 50,
+            "id": 1832445
+        },
+        "1848745": {
+            "local_score": 0,
+            "id": 1848745,
+            "last_star_ts": 0,
+            "name": "Joao Rodrigues",
+            "completion_day_level": {
+
+            },
+            "global_score": 0,
+            "stars": 0
+        },
+        "1852639": {
+            "stars": 0,
+            "completion_day_level": {
+
+            },
+            "global_score": 0,
+            "name": "luisfilipedcc",
+            "last_star_ts": 0,
+            "local_score": 0,
+            "id": 1852639
+        },
+        "2339302": {
+            "last_star_ts": 1670069735,
+            "local_score": 42,
+            "id": 2339302,
+            "name": "Ricardo Caetano",
+            "completion_day_level": {
+                "1": {
+                    "1": {
+                        "star_index": 36787,
+                        "get_star_ts": 1669900697
+                    },
+                    "2": {
+                        "star_index": 39397,
+                        "get_star_ts": 1669901478
+                    }
+                },
+                "2": {
+                    "1": {
+                        "get_star_ts": 1669993173,
+                        "star_index": 370945
+                    },
+                    "2": {
+                        "get_star_ts": 1670008597,
+                        "star_index": 429275
+                    }
+                },
+                "3": {
+                    "1": {
+                        "star_index": 597354,
+                        "get_star_ts": 1670066337
+                    },
+                    "2": {
+                        "star_index": 610716,
+                        "get_star_ts": 1670069735
+                    }
+                }
+            },
+            "global_score": 0,
+            "stars": 6
+        },
+        "2432489": {
+            "stars": 4,
+            "completion_day_level": {
+                "1": {
+                    "1": {
+                        "star_index": 79786,
+                        "get_star_ts": 1669912382
+                    },
+                    "2": {
+                        "star_index": 80180,
+                        "get_star_ts": 1669912488
+                    }
+                },
+                "2": {
+                    "1": {
+                        "get_star_ts": 1669964159,
+                        "star_index": 234059
+                    },
+                    "2": {
+                        "get_star_ts": 1669964416,
+                        "star_index": 235153
+                    }
+                }
+            },
+            "global_score": 0,
+            "name": "Alexandre Meneses",
+            "last_star_ts": 1669964416,
+            "id": 2432489,
+            "local_score": 30
+        },
+        "2434692": {
+            "completion_day_level": {
+                "1": {
+                    "1": {
+                        "get_star_ts": 1669908581,
+                        "star_index": 65873
+                    },
+                    "2": {
+                        "get_star_ts": 1669909019,
+                        "star_index": 67531
+                    }
+                },
+                "2": {
+                    "1": {
+                        "get_star_ts": 1670034760,
+                        "star_index": 486531
+                    },
+                    "2": {
+                        "get_star_ts": 1670036072,
+                        "star_index": 487850
+                    }
+                },
+                "3": {
+                    "1": {
+                        "get_star_ts": 1670044975,
+                        "star_index": 508809
+                    },
+                    "2": {
+                        "get_star_ts": 1670045753,
+                        "star_index": 515964
+                    }
+                }
+            },
+            "name": "Vasco Teixeira",
+            "global_score": 0,
+            "stars": 6,
+            "last_star_ts": 1670045753,
+            "id": 2434692,
+            "local_score": 42
+        }
+    }
+}
--- main.py
+++ main.py
@@ -1,9 +1,30 @@
+from datetime import datetime
+
 from day1 import resolve_day1
 from day2 import resolve_day2
 from day3 import resolve_day3
+from inputs.leaderboard import leaderboard
 
 DAY = 0
+LEADERBOARD = 0
 
+
+def show_leaderboard():
+    for member in leaderboard['members']:
+        # print(leaderboard['members'][member])
+        m = leaderboard['members'][member]
+        print(m["name"])
+        print("Stars:", m["stars"], " Score:", m["local_score"])
+        if len(m['completion_day_level']) > 0:
+            for x in m['completion_day_level']:
+                print("Day", x)
+                if "get_star_ts" in m['completion_day_level'][x]["1"]:
+                    ts = int(m['completion_day_level'][x]["1"]["get_star_ts"])
+                    print(datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S'))
+
+                if "get_star_ts" in m['completion_day_level'][x]["2"]:
+                    ts = int(m['completion_day_level'][x]["2"]["get_star_ts"])
+                    print(datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S'))
 def resolve_all():
     resolve_day1()
     resolve_day2()
@@ -11,6 +32,8 @@ def resolve_all():
 
 
 def resolve(day):
+    if LEADERBOARD:
+        show_leaderboard()
     if day == 0:
         resolve_all()
     if day == 1:
